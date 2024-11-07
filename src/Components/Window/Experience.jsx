e737439f1a8a85c10ac49ebe4328f76334389801
JeenVerawood <163981873+JeenVerawood@users.noreply.github.com>
2024-11-07 23:12:01 +0700

    fix experience

--- src/Components/Window/Experience.jsx
+++ src/Components/Window/Experience.jsx
@@ -1,5 +1,11 @@
 import React, { useRef, useEffect, useState } from 'react';
 import img1 from "../img-main/img1/1.png"
+import img2 from "../img-main/img3/1.png"
+import img3 from "../img-main/img4/3.png"
+import img4 from "../img-main/img1/Img1.png"
+
+
+
 // import img1sec1 from "../img-main/img1/1.png";
 // import img1sec2 from "../img-main/img3/1.png";
 // import img1sec3 from "../img-main/img4/3.png";
@@ -59,19 +65,19 @@ export default function Experience() {
                     </div>
                     <div className='h-92 w-72 mr-2 flex border-2 border-gray-400 flex-col items-center p-4 hover:scale-110 transition-all duration-500'>
                         <div className='border-2 border-gray-400 '>
-                            {/* <img src={img1sec1} alt="" className='w-64 h-64 bg-white'/> */}
+                            <img src={img2} alt="" className='w-64 h-64 bg-white'/>
                         </div>
                         <p className='text-center mt-2 text-white'>Medal award from the regional SMTE project competition using the ARDUINO board.</p>
                     </div>
                     <div className='h-92 w-72 mr-2 flex border-2 border-gray-400 flex-col items-center p-4 hover:scale-110 transition-all duration-500'>
                         <div className='border-2 border-gray-400 '>
-                            {/* <img src={img1sec2} alt="" className='w-64 h-64 bg-white'/> */}
+                            <img src={img3} alt="" className='w-64 h-64 bg-white'/>
                         </div>
                         <p className='text-center mt-2 text-white'>Silver Medal in the Arts and Crafts Competition in the Mixed Robot Competition.</p>
                     </div>
                     <div className='h-92 w-72 mr-2 flex border-2 border-gray-400 flex-col items-center p-4 hover:scale-110 transition-all duration-500'>
                         <div className='border-2 border-gray-400 '>
-                            {/* <img src={img1sec3} alt="" className='w-64 h-64 bg-white'/> */}
+                            <img src={img3} alt="" className='w-64 h-64 bg-white'/>
                         </div>
                         <p className='text-center mt-2 text-white'>Passed entry into the Junior Web Master Camp project, design category.</p>
                     </div>
